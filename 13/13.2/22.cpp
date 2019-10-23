dce1cb70784bfdfdae9b29b9a2d0ba1cc601ee2c
zhuiyicc <46620869+zhuiyicc@users.noreply.github.com>
2019-10-23 12:38:28 +0800

    Create 22.cpp

---
+++ 13/13.2/22.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class HasPtr{
+    public:
+        HasPtr(const int xx = 0,const string& s = ""):x(xx){
+            strp = new string(s);
+        }
+        HasPtr(HasPtr& a):x(a.x){
+            strp = new string(*a.strp);
+        }
+        HasPtr& operator=(const HasPtr& a){
+            x = a.x;
+            strp = new string(*a.strp);
+            return *this;
+        }
+        friend ostream& operator<<(ostream& out,const HasPtr& a){
+            return out << hex << a.strp <<" "<< *a.strp << endl;
+        }
+    private:
+        int x;
+        string *strp;
+};
+int main(){
+    int t = 1;
+    string s = "123";
+    HasPtr p1(t,s);
+    HasPtr p2 = p1;
+    HasPtr p3;
+    p3 = p2;
+    cout << p1 << endl;
+    cout << p2 << endl;
+    cout << p3 << endl;
+    return 0;
+}
